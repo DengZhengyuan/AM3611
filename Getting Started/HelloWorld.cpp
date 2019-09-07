9703ca34450aa2a41bd860980ef17dbcc4053a8d
Zach Deng <dengzhengyuan2240@gmail.com>
2019-09-06 23:04:31 -0400

    success to run the first program

--- Getting Started/HelloWorld.cpp
+++ Getting Started/HelloWorld.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 /* This program prints out the message "Hello World" and can be used     */
 /* as a basic test of the setup of the compiler and run time environment */
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-  // Print to screen (line terminal) "Hello World" followed by a newline character
-  std::cout << "Hello World\n";
-
-  return 0;
+    // Print to screen (line terminal) "Hello World" followed by a newline character
+    std::cout << "Hello World\n";
+    
+    return 0;
 }
