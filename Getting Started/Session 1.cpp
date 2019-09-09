90f08426960936065cd03e93df7e8c51a28fb792
Zach Deng <dengzhengyuan2240@gmail.com>
2019-09-09 11:32:33 -0400

    creat session 1.cpp

--- Getting Started/Session 1.cpp
+++ Getting Started/Session 1.cpp
@@ -1,11 +1,3 @@
-//
-//  Session 1.cpp
-//  AM3611
-//
-//  Created by Zhengyuan Deng on 2019/9/9.
-//  Copyright © 2019 Zhengyuan Deng. All rights reserved.
-//
-
 #include <iostream>
 
 int main(int argc, char const *arg[])
@@ -14,5 +6,8 @@ int main(int argc, char const *arg[])
     int a = 8;
     float b = 3.1415926;
     double c = 3.1231242324;
-    return 0;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "c = " << c << std::endl; 
+    return 0; 
 }
