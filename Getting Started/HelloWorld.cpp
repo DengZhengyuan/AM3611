4299634cfff4202444f9d70a9303eb92488a27ab
Zach Deng <dengzhengyuan2240@gmail.com>
2019-09-09 11:16:55 -0400

    switch to vs code

--- Getting Started/HelloWorld.cpp
+++ Getting Started/HelloWorld.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 /* This program prints out the message "Hello World" and can be used     */
 /* as a basic test of the setup of the compiler and run time environment */
-int main(int argc, char* argv[])
+int main(int argc, char** arg)
 {
-    // Print to screen (line terminal) "Hello World" followed by a newline character
+    /* Print to screen (line terminal) "Hello World"
+      followed by a newline character */
     std::cout << "Hello World\n";
     
     return 0;
---
+++ Getting Started/Session 1.cpp
@@ -0,0 +1,18 @@
+//
+//  Session 1.cpp
+//  AM3611
+//
+//  Created by Zhengyuan Deng on 2019/9/9.
+//  Copyright © 2019 Zhengyuan Deng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, char const *arg[])
+{
+    // define the variables 
+    int a = 8;
+    float b = 3.1415926;
+    double c = 3.1231242324;
+    return 0;
+}
