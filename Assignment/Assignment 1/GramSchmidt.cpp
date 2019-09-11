a468a868f35b8c11e3955916ccc539c877f47647
Zach Deng <dengzhengyuan2240@gmail.com>
2019-09-10 22:39:20 -0400

    lack two checkings in GramSchmid.cpp

--- Assignment/Assignment 1/GramSchmidt.cpp
+++ Assignment/Assignment 1/GramSchmidt.cpp
@@ -11,113 +11,234 @@
 #include <iostream>
 #include <cmath>
 
+/* ---------------------------------------------- */
+/*                   FUNCTIONS                    */
+/* ---------------------------------------------- */
+
 // the dot production
 double dot_production(int size, double *vec_1, double *vec_2);
 
 // the Euclidean norm
 double Euclidean_norm(int size, double *array);
 
-// the projection of vec_a to vec_u
-double* projection(int size, double *vec_u, double *vec_a);
+// the projection of vec_a onto vec_u
+double *projection(int size, double *vec_u, double *vec_a);
+
+// transpose a square matrix and calculate the
+// production of it and a vector
+double *matrixT_x_vector(int size, double **matrix, double *vector);
+
+// production of two matrixes
+double **matrix_x_matrix(int size, double **m_1, double **m_2);
 
 // function to create a square matrix
-double** create_a_square_matrix(int size);
+double **create_a_square_matrix(int size);
+
+// function to create an upper triangle matrix
+double **create_an_upper_triangle_matrix(int size);
 
 // function to free the memory of a matrix
-void delete_matrix(int size, double** matrix); 
+void delete_matrix(int size, double **matrix);
 
 /* ---------------------------------------------- */
-/*                                                */
+/*                 MAIN FUNCTION                  */
 /* ---------------------------------------------- */
 
 int main(int argc, char *argv[])
-{   
-    // define the size of matrix A
-    int size = 2; 
+{
+    /* ----------- start with matrix A ----------- */
 
-    // define the matrixes of A, U, and e
-    double **A, **U, **Q; 
+    // input the size of matrix A
+    int size;
+    std::cout << "Please enter the dimension of matrix A. \n ";
+    std::cout << "(If A is a (2x2) matrix, please enter 2). \n ";
+    std::cin >> size;
+    std::cout << "\n";
+
+    // input the matrix A
+    double **A;
     A = create_a_square_matrix(size);
-    U = create_a_square_matrix(size);
-    Q = create_a_square_matrix(size);
-
-    // define the upper triangular matrix R
-    double **R; 
-    R = new double* [size];
-    for (int i = 0; i < size; i++) {
-        R[i] = new double [size - i]; 
-    }
-
-    // define the vector of projection
-    double *Proj, *b;
-    Proj = new double [size]; 
-    b = new double [size]; 
-
-    // initialize the matrix A and vector b
-    A[0][0] = 3.; A[1][0] = 7.;
-    A[0][1] = 1.; A[1][1] = 9.;
-    b[0] = 0.;    b[1] = 5.;
-
-    // calculate the matrix U
-    U[0] = A[0];
-
-    for (int i = 1; i < size; i++) { 
-        U[i] = A[i]; 
-        for (int j = 0; j < i; j++) {
-            Proj = projection(size, U[j], A[i]);
-            for (int k = 0; k < size; k++) {
-                U[i][k] -= Proj[k];
-            } 
-            delete[] Proj; 
+    std::cout << "Please enter the all elements of matrix A. \n";
+    std::cout << "(Please enter the column vector in turn). \n ";
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            std::cin >> A[i][j];
         }
     }
-    
+    std::cout << "\n";
+
+    // here should assert if A is singular.
+
+    /* ----------- Matrix U, Q, and R ----------- */
+
+    // define the matrixes of U, Q and R
+    double **U, **Q, **R;
+    U = create_a_square_matrix(size);
+    Q = create_a_square_matrix(size);
+    R = create_an_upper_triangle_matrix(size);
+
+    // calculate the matrix U
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            U[i][j] = A[i][j];
+        }
+
+        for (int j = 0; j < i; j++)
+        {
+            double *Proj;
+            Proj = new double[size];
+            Proj = projection(size, U[j], A[i]);
+            for (int k = 0; k < size; k++)
+            {
+                U[i][k] -= Proj[k];
+            }
+            delete[] Proj;
+        }
+    }
+
     // calculate the matrix Q
-    for (int i = 0; i < size; i++) {         
-        for (int j = 0; j < size; j++) {
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
             Q[i][j] = U[i][j] / Euclidean_norm(size, U[i]);
         }
     }
 
+    delete_matrix(size, U);
+
     // calculate the matrix R
-    for (int j = 0; j < size; j++) {
-        for (int i = 0; i < (j+1); i++) {
-            R[j][i] = dot_production(size, Q[i], A[j]); 
+    for (int j = 0; j < size; j++)
+    {
+        for (int i = 0; i < (j + 1); i++)
+        {
+            R[j][i] = dot_production(size, Q[i], A[j]);
         }
     }
-    
-
-    for (int j = 0; j < size; j++) {
-        for (int i = 0; i < (j+1); i++) {
-            std::cout << R[j][i] << std::endl;
-        }
-    }
-
-    /* ------------------------------------ */
 
     delete_matrix(size, A);
-    delete_matrix(size, U); 
-    delete_matrix(size, Q); 
 
-    // free the memory of matrix R
-    for (int i = 0; i < size; i++) {
-        delete[] R[size - i]; 
+    double **QR;
+    QR = create_a_square_matrix(size);
+    QR = matrix_x_matrix(size, Q, R);
+
+    // print out the matrix Q, R and QR
+    {
+        std::cout << "The matrix Q is " << std::endl;
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                std::cout << Q[j][i] << "  ";
+            }
+            std::cout << "\n";
+        }
+        std::cout << "\n";
+
+        // print out the matrix R
+        std::cout << "The matrix R is " << std::endl;
+        for (int j = 0; j < size; j++)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (j < (i + 1))
+                {
+                    std::cout << R[i][j] << "  ";
+                }
+                else
+                {
+                    std::cout << 0 << "        ";
+                }
+            }
+            std::cout << "\n";
+        }
+        std::cout << "\n";
+
+        std::cout << "The matrix QR is " << std::endl;
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                std::cout << QR[j][i] << "  ";
+            }
+            std::cout << "\n";
+        }
+        std::cout << "\n";
     }
-    delete[] R;
 
+    /* ------------ Vector b ------------ */
+
+    double *b;
+    b = new double[size];
+    std::cout << "Please enter the all elements of vector b. \n";
+    for (int i = 0; i < size; i++)
+    {
+        std::cin >> b[i];
+    }
+    std::cout << "\n";
+
+    /* ------ calculate the results ------ */
+
+    // calculate the production of matrix QT and vector b
+    double *QTb;
+    QTb = new double[size];
+    QTb = matrixT_x_vector(size, Q, b);
+
+    delete_matrix(size, Q);
+
+    // calculate the final results
+    double *final_result;
+    final_result = new double[size];
+
+    double R_x, numerator;
+    for (int i = (size - 1); i >= 0; i--)
+    {
+        if (i == (size - 1))
+        {
+            R_x = 0.;
+        }
+        else
+        {
+            for (int j = (size - 1); j > i; j--)
+            {
+                R_x += (R[j][i] * final_result[j]);
+            }
+        }
+        numerator = QTb[i] - R_x;
+        R_x = 0.;
+        final_result[i] = numerator / R[i][i];
+        numerator = 0.;
+    }
     delete[] b;
+    delete[] QTb;
+    delete_matrix(size, R);
+
+    // print out the final results
+    std::cout << "The final results are \n";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << final_result[i]
+                  << "\n";
+    }
+    std::cout << "\n";
+    delete[] final_result;
 
     return 0;
 }
 
 /* ---------------------------------------------- */
-/*                                                */
+/*                DEFINE FUNCTIONS                */
 /* ---------------------------------------------- */
 
 double dot_production(int size, double *vec_1, double *vec_2)
 {
-    double result;
-    for (int i = 0; i < size; i++) {
+    double result = 0.;
+    for (int i = 0; i < size; i++)
+    {
         result += vec_1[i] * vec_2[i];
     }
     return result;
@@ -125,46 +246,100 @@ double dot_production(int size, double *vec_1, double *vec_2)
 
 double Euclidean_norm(int size, double *array)
 {
-    double result,
-           sum;
-    for (int i = 0; i < size; i++) {
+    double result = 0.,
+           sum = 0.;
+    for (int i = 0; i < size; i++)
+    {
         sum += pow(array[i], 2);
     }
     result = sqrt(sum);
     return result;
 }
 
-double* projection(int size, double *vec_u, double *vec_a)
+double *projection(int size, double *vec_u, double *vec_a)
 {
-    double *vec; 
-    vec = new double [size];
+    double *vec;
+    vec = new double[size];
 
-    double coefficient;
+    double coefficient = 0.;
 
     coefficient = dot_production(size, vec_u, vec_a) /
-                  dot_production(size, vec_u, vec_u); 
+                  dot_production(size, vec_u, vec_u);
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         vec[i] = vec_u[i] * coefficient;
     }
 
     return vec;
 }
 
-double** create_a_square_matrix(int size)
+double *matrixT_x_vector(int size, double **matrix, double *vector)
 {
-    double** matrix; 
-    matrix = new double* [size];
-    for (int i = 0; i < size; i++) {
-        matrix[i] = new double [size];
+    double *result_vec;
+    result_vec = new double[size];
+    double sum = 0.;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            sum += matrix[i][j] * vector[j];
+        }
+        result_vec[i] = sum;
+        sum = 0.;
     }
-    return matrix; 
+    return result_vec;
 }
 
-void delete_matrix(int size, double** matrix)
+double **create_a_square_matrix(int size)
 {
-    for (int i = 1; i < size; i++) {
+    double **matrix;
+    matrix = new double *[size];
+    for (int i = 0; i < size; i++)
+    {
+        matrix[i] = new double[size];
+    }
+    return matrix;
+}
+
+double **create_an_upper_triangle_matrix(int size)
+{
+    double **matrix;
+    matrix = new double *[size];
+    for (int i = 0; i < size; i++)
+    {
+        matrix[i] = new double[size - 1];
+    }
+    return matrix;
+}
+
+void delete_matrix(int size, double **matrix)
+{
+    for (int i = 0; i < size; i++)
+    {
         delete[] matrix[i];
     }
     delete[] matrix;
+}
+
+double **matrix_x_matrix(int size, double **m_1, double **m_2)
+{
+    double **result_m;
+    result_m = create_a_square_matrix(size);
+    double sum = 0.;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            for (int k = 0; k < size; k++)
+            {
+                sum += m_1[k][j] * m_2[i][k];
+            }
+            result_m[i][j] = sum;
+            sum = 0.;
+        }
+    }
+
+    return result_m;
 }
--- Assignment/Assignment 1/problem1_3.cpp
+++ Assignment/Assignment 1/problem1_3.cpp
@@ -13,51 +13,53 @@
 #include <cmath>
 
 // define the function to calculate the dot production
-double dot_production(double *array_1, double *array_2)
-{
-    double result;
-    if (sizeof(array_1) == sizeof(array_2))
-    {
-        for (int i = 0; i < sizeof(array_1); i++)
-            result += array_1[i] * array_2[i];
-    }
-    else
-    {
-        std::cerr << "ERROR: the sizes of vectors x and y are different. ";
-    }
-    return result;
-}
+double dot_production(int size, double *array_1, double *array_2);
 
 // define the function to calculate the Euclidean norm
-double Euclidean_norm(double *array)
-{
-    double result,
-        sum;
-    for (int i = 0; i < sizeof(array); i++)
-        sum += pow(array[i], 2);
-    result = sqrt(sum);
-    return result;
-}
+double Euclidean_norm(int size, double *array);
 
 int main(int argc, char *argv[])
 {
     // define the vectors x and y
+    int size = 4; 
     double array_x[4] = {3.14, 2.72, 1.62, 0.};
     double array_y[4] = {pow(2, 8) - 1,
-                         (sqrt(13) + 3) / (2),
-                         log(2),
-                         sin(M_PI / 2)};
+                            (sqrt(13) + 3) / (2),
+                            log(2),
+                            sin(M_PI / 2)};
 
     // print out the result of dot production
     std::cout << "The dot production of vectors x and y is "
-              << dot_production(array_x, array_y)
+              << dot_production(size, array_x, array_y)
               << ". "
               << std::endl;
     // print out the result of Euclidean norm
     std::cout << "The Euclidean norm of vector x is "
-              << Euclidean_norm(array_x)
+              << Euclidean_norm(size, array_x)
+              << ". "
+              << std::endl;
+    std::cout << "The Euclidean norm of vector y is "
+              << Euclidean_norm(size, array_y)
               << ". "
               << std::endl;
 
     return 0;
+}
+
+double dot_production(int size, double *array_1, double *array_2)
+{
+    double result = 0.;
+    for (int i = 0; i < size; i++)
+        result += array_1[i] * array_2[i];
+    return result;
+}
+
+double Euclidean_norm(int size, double *array)
+{
+    double result = 0.,
+           sum = 0.;
+    for (int i = 0; i < size; i++)
+        sum += pow(array[i], 2);
+    result = sqrt(sum);
+    return result;
 }
