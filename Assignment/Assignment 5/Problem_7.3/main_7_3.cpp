11bf6e8f2ce7239b64e9d0f77cfd2ac0deabe0c3
Zach Deng <dengzhengyuan2240@gmail.com>
2019-11-21 00:08:58 -0500

    update the CMD for problem 7.3

--- Assignment/Assignment 5/Problem_7.3/main_7_3.cpp
+++ Assignment/Assignment 5/Problem_7.3/main_7_3.cpp
@@ -1,3 +1,14 @@
+/*
+    NAME:   Zhengyuan Deng
+    STU #:  251006745
+    DATA:   Nov. 18, 2019
+    PROB #: Problem 7.3
+    DESCRIPTION:
+            add Euler method and Runge Kutta method into AbstractOdeSover to solve the ODE
+    CMD:    
+            g++ -Wall -O -c AbstractOdeSolver.cpp && g++ -Wall -O -c EulerMethod.cpp && g++ -Wall -O -c RungeKutta.cpp && g++ -Wall -lm -O -o main_7_3.out main_7_3.cpp AbstractOdeSolver.o EulerMethod.o RungeKutta.o && ./main_7_3.out
+*/
+
 #include <iostream>
 #include "EulerMethod.h"
 #include "AbstractOdeSolver.h"
